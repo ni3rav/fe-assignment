af5cdf9fd00319f9fcc49ccb82f5bae0e2b9cdfa
ni3rav <mahetan35@gmail.com>
2025-05-23 10:01:29 +0530

    add: next auth with google signin

---
+++ components/auth-button.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+
+export function AuthButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div className="flex items-center gap-4">
+        <span>Signed in as {session.user?.email}</span>
+        <Button onClick={() => signOut()}>Sign out</Button>
+      </div>
+    );
+  }
+  return <Button onClick={() => signIn("google")}>Sign in with Google</Button>;
+}
